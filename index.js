bf9087b705c53e9ed27d59df248123a69ef20800
Riki Liir <TA-20E-344@tptlive.ee>
2022-09-28 13:59:48 +0300

    added crud

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+//crud api endpoints for users
+app.get('/users', (req, res) => {
+  res.send('get all users')
+})
+app.get('/users/:id', (req, res) => {
+  res.send('get user by id')
+})
+app.post('/users', (req, res) => {
+  res.send('create user')
+})
+app.put('/users/:id', (req, res) => {
+  res.send('update user by id')
+})
+app.delete('/users/:id', (req, res) => {
+  res.send('delete user by id')
+})
+    
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
